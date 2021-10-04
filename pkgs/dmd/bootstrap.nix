f9c045781a3949ac639da52ec93afc96c88e243b
Petar Kirov <petar.p.kirov@gmail.com>
2021-10-05 00:08:36 +0300

    build(nix): Upgrade `dmd-binary` package to `2.097.2`

--- pkgs/dmd/bootstrap.nix
+++ pkgs/dmd/bootstrap.nix
@@ -1,9 +1,9 @@
 { callPackage }:
 callPackage ./binary.nix {
-  version = "2.090.1";
+  version = "2.097.2";
   hashes = {
-    # Get these from `nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.090.1/dmd.2.090.1.linux.tar.xz` etc..
-    osx = "0rbn7j4dr3q0y09fblpj999bi063pi4230rqd5xgd3gwxxa0cz7l";
-    linux = "1vk6lsvd6y7ccvffd23yial4ig90azaxf2rxc6yvidqd1qhan807";
+    # get these from `nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.097.2/dmd.2.097.2.linux.tar.xz` etc..
+    osx = "021mwsssgpjsil5sjy47jfclrkq6155bm0gillpcdz7rv1vjnfhw";
+    linux = "04ckf54zgclkfdjr2palwiz68bdn7jqiynk37k7njwxc8i4bg066";
   };
 }
