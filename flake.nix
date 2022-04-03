18ffc11f1113f57e874d8043a818921bacda46f6
Petar Kirov <petar.p.kirov@gmail.com>
2022-04-03 18:22:54 +0300

    build(nix): Add Nix Flakes & nix-direnv dev shell

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nix packages for D projects";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "dlang-nix";
+      shell = ./shell.nix;
+    };
+}
