4ba000da6a2b7c501eed8b97bad5758bbdbbb833
Ate Eskola <ajieskola@gmail.com>
2023-04-08 20:25:05 +0300

    Porting improvements from OldDDerivations, first part

---
+++ pkgs/dmd/2.102.nix
@@ -0,0 +1,5 @@
+import ./generic.nix {
+  version = "2.102.2";
+  dmdSha256 = "sha256-der9nb31hJ+K1aJZdzIgs8+eRgVVsH97QnYEnVbKUws=";
+  phobosSha256 = "sha256-SracmUm2aY/LDCyDqYuVS39pCbwO8UCL3TSB0CVHpHE=";
+}
--- pkgs/dmd/bootstrap.nix
+++ pkgs/dmd/bootstrap.nix
@@ -1,11 +1,11 @@
 {callPackage}:
 callPackage ./binary.nix {
-  version = "2.098.0";
+  version = "2.090.1";
   hashes = {
     # get these from:
-    #   nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.098.0/dmd.2.098.0.linux.tar.xz
-    #   nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.098.0/dmd.2.098.0.osx.tar.xz
-    linux = "0i81vg218n3bbj2s2x6kl6xqdw3vajz74ynpk2vjhy6lkzjya10i";
-    osx = "0hpkqwj80ydbmssch9a4gzknnrbmyw37g43ygrj9ljcx8baam03p";
+    #   nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.090.1/dmd.2.090.1.linux.tar.xz
+    #   nix-prefetch-url http://downloads.dlang.org/releases/2.x/2.090.1/dmd.2.090.1.osx.tar.xz
+    linux = "1vk6lsvd6y7ccvffd23yial4ig90azaxf2rxc6yvidqd1qhan807";
+    osx = "0rbn7j4dr3q0y09fblpj999bi063pi4230rqd5xgd3gwxxa0cz7l";
   };
 }
