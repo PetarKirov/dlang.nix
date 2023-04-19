37b3575f82d76821c9c8c3ed6ac57207d52c7c7f
Petar Kirov <petar.p.kirov@gmail.com>
2023-04-19 15:16:37 +0300

    style(nix): Format files with `alejandra`

--- pkgs/dmd/binary.nix
+++ pkgs/dmd/binary.nix
@@ -1,71 +1,87 @@
-{ lib, stdenv, fetchurl, curl, tzdata, autoPatchelfHook, fixDarwinDylibNames, glibc
-, version, hashes }:
-
-let
+{
+  lib,
+  stdenv,
+  fetchurl,
+  curl,
+  tzdata,
+  autoPatchelfHook,
+  fixDarwinDylibNames,
+  glibc,
+  version,
+  hashes,
+}: let
   inherit (stdenv) hostPlatform;
-  OS = if hostPlatform.isDarwin then "osx" else hostPlatform.parsed.kernel.name;
+  OS =
+    if hostPlatform.isDarwin
+    then "osx"
+    else hostPlatform.parsed.kernel.name;
   MODEL = toString hostPlatform.parsed.cpu.bits;
-in stdenv.mkDerivation {
-  pname = "dmd-bootstrap";
-  inherit version;
+in
+  stdenv.mkDerivation {
+    pname = "dmd-bootstrap";
+    inherit version;
 
-  src = fetchurl rec {
-    name = "dmd.${version}.${OS}.tar.xz";
-    url = "http://downloads.dlang.org/releases/2.x/${version}/${name}";
-    sha256 = hashes.${OS} or (throw "missing bootstrap sha256 for OS ${OS}");
-  };
+    src = fetchurl rec {
+      name = "dmd.${version}.${OS}.tar.xz";
+      url = "http://downloads.dlang.org/releases/2.x/${version}/${name}";
+      sha256 = hashes.${OS} or (throw "missing bootstrap sha256 for OS ${OS}");
+    };
 
-  dontConfigure = true;
-  dontBuild = true;
+    dontConfigure = true;
+    dontBuild = true;
 
-  nativeBuildInputs = lib.optionals hostPlatform.isLinux [
-    autoPatchelfHook
-  ] ++ lib.optionals hostPlatform.isDarwin [
-    fixDarwinDylibNames
-  ];
-  propagatedBuildInputs = [
-    curl
-    tzdata
-  ] ++ lib.optionals hostPlatform.isLinux [
-    glibc
-  ];
+    nativeBuildInputs =
+      lib.optionals hostPlatform.isLinux [
+        autoPatchelfHook
+      ]
+      ++ lib.optionals hostPlatform.isDarwin [
+        fixDarwinDylibNames
+      ];
+    propagatedBuildInputs =
+      [
+        curl
+        tzdata
+      ]
+      ++ lib.optionals hostPlatform.isLinux [
+        glibc
+      ];
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    mkdir -p $out
+      mkdir -p $out
 
-    # try to copy model-specific binaries into bin first
-    mv ${OS}/bin${MODEL} $out/bin || true
+      # try to copy model-specific binaries into bin first
+      mv ${OS}/bin${MODEL} $out/bin || true
 
-    mv src license.txt ${OS}/* $out/
+      mv src license.txt ${OS}/* $out/
 
-    # move man into place
-    mkdir -p $out/share
-    mv man $out/share/
+      # move man into place
+      mkdir -p $out/share
+      mv man $out/share/
 
-    # move docs into place
-    mkdir -p $out/share/doc
-    mv html/d $out/share/doc/
+      # move docs into place
+      mkdir -p $out/share/doc
+      mv html/d $out/share/doc/
 
-    # fix paths in dmd.conf (one level less)
-    substituteInPlace $out/bin/dmd.conf --replace "/../../" "/../"
+      # fix paths in dmd.conf (one level less)
+      substituteInPlace $out/bin/dmd.conf --replace "/../../" "/../"
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  # Stripping on Darwin started to break libphobos2.a
-  # Undefined symbols for architecture x86_64:
-  #   "_rt_envvars_enabled", referenced from:
-  #       __D2rt6config16rt_envvarsOptionFNbNiAyaMDFNbNiQkZQnZQq in libphobos2.a(config_99a_6c3.o)
-  dontStrip = hostPlatform.isDarwin;
+    # Stripping on Darwin started to break libphobos2.a
+    # Undefined symbols for architecture x86_64:
+    #   "_rt_envvars_enabled", referenced from:
+    #       __D2rt6config16rt_envvarsOptionFNbNiAyaMDFNbNiQkZQnZQq in libphobos2.a(config_99a_6c3.o)
+    dontStrip = hostPlatform.isDarwin;
 
-  meta = with lib; {
-    description = "Digital Mars D Compiler Package";
-    # As of 2.075 all sources and binaries use the boost license
-    license = licenses.boost;
-    maintainers = [ maintainers.lionello ];
-    homepage = "https://dlang.org/";
-    platforms = [ "x86_64-darwin" "i686-linux" "x86_64-linux" ];
-  };
-}
+    meta = with lib; {
+      description = "Digital Mars D Compiler Package";
+      # As of 2.075 all sources and binaries use the boost license
+      license = licenses.boost;
+      maintainers = [maintainers.lionello];
+      homepage = "https://dlang.org/";
+      platforms = ["x86_64-darwin" "i686-linux" "x86_64-linux"];
+    };
+  }
--- pkgs/dmd/generic.nix
+++ pkgs/dmd/generic.nix
@@ -1,250 +1,256 @@
-{ version
-, dmdSha256
-, druntimeSha256
-, phobosSha256
-}:
-
-{ stdenv
-, lib
-, fetchFromGitHub
-, makeWrapper
-, which
-, writeTextFile
-, curl
-, tzdata
-, gdb
-, Foundation
-, callPackage
-, targetPackages
-, fetchpatch
-, bash
-, installShellFiles
-, git
-, unzip
-, HOST_DMD ? "${callPackage ./bootstrap.nix { }}/bin/dmd"
-}:
-
-let
+{
+  version,
+  dmdSha256,
+  druntimeSha256,
+  phobosSha256,
+}: {
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  makeWrapper,
+  which,
+  writeTextFile,
+  curl,
+  tzdata,
+  gdb,
+  Foundation,
+  callPackage,
+  targetPackages,
+  fetchpatch,
+  bash,
+  installShellFiles,
+  git,
+  unzip,
+  HOST_DMD ? "${callPackage ./bootstrap.nix {}}/bin/dmd",
+}: let
   dmdConfFile = writeTextFile {
     name = "dmd.conf";
-    text = (lib.generators.toINI { } {
+    text = lib.generators.toINI {} {
       Environment = {
         DFLAGS = ''-I@out@/include/dmd -L-L@out@/lib -fPIC ${lib.optionalString (!targetPackages.stdenv.cc.isClang) "-L--export-dynamic"}'';
       };
-    });
+    };
   };
 
   bits = builtins.toString stdenv.hostPlatform.parsed.cpu.bits;
   osname =
-    if stdenv.isDarwin then
-      "osx"
-    else
-      stdenv.hostPlatform.parsed.kernel.name;
+    if stdenv.isDarwin
+    then "osx"
+    else stdenv.hostPlatform.parsed.kernel.name;
 
   pathToDmd = "\${NIX_BUILD_TOP}/dmd/generated/${osname}/release/${bits}/dmd";
 in
+  stdenv.mkDerivation rec {
+    pname = "dmd";
+    inherit version;
 
-stdenv.mkDerivation rec {
-  pname = "dmd";
-  inherit version;
+    enableParallelBuilding = true;
 
-  enableParallelBuilding = true;
+    srcs = [
+      (fetchFromGitHub {
+        owner = "dlang";
+        repo = "dmd";
+        rev = "v${version}";
+        sha256 = dmdSha256;
+        name = "dmd";
+      })
+      (fetchFromGitHub {
+        owner = "dlang";
+        repo = "druntime";
+        rev = "v${version}";
+        sha256 = druntimeSha256;
+        name = "druntime";
+      })
+      (fetchFromGitHub {
+        owner = "dlang";
+        repo = "phobos";
+        rev = "v${version}";
+        sha256 = phobosSha256;
+        name = "phobos";
+      })
+    ];
 
-  srcs = [
-    (fetchFromGitHub {
-      owner = "dlang";
-      repo = "dmd";
-      rev = "v${version}";
-      sha256 = dmdSha256;
-      name = "dmd";
-    })
-    (fetchFromGitHub {
-      owner = "dlang";
-      repo = "druntime";
-      rev = "v${version}";
-      sha256 = druntimeSha256;
-      name = "druntime";
-    })
-    (fetchFromGitHub {
-      owner = "dlang";
-      repo = "phobos";
-      rev = "v${version}";
-      sha256 = phobosSha256;
-      name = "phobos";
-    })
-  ];
+    sourceRoot = ".";
 
-  sourceRoot = ".";
+    # https://issues.dlang.org/show_bug.cgi?id=19553
+    hardeningDisable = ["fortify"];
 
-  # https://issues.dlang.org/show_bug.cgi?id=19553
-  hardeningDisable = [ "fortify" ];
+    patches =
+      lib.optionals (lib.versionOlder version "2.088.0") [
+        # Migrates D1-style operator overloads in DMD source, to allow building with
+        # a newer DMD
+        (fetchpatch {
+          url = "https://github.com/dlang/dmd/commit/c4d33e5eb46c123761ac501e8c52f33850483a8a.patch";
+          stripLen = 1;
+          extraPrefix = "dmd/";
+          sha256 = "sha256-N21mAPfaTo+zGCip4njejasraV5IsWVqlGR5eOdFZZE=";
+        })
+      ]
+      ++ lib.optionals (lib.versionOlder version "2.092.2") [
+        # Fixes C++ tests that compiled on older C++ but not on the current one
+        (fetchpatch {
+          url = "https://github.com/dlang/druntime/commit/438990def7e377ca1f87b6d28246673bb38022ab.patch";
+          stripLen = 1;
+          extraPrefix = "druntime/";
+          sha256 = "sha256-/pPKK7ZK9E/mBrxm2MZyBNhYExE8p9jz8JqBdZSE6uY=";
+        })
+      ];
 
-  patches = lib.optionals (lib.versionOlder version "2.088.0") [
-    # Migrates D1-style operator overloads in DMD source, to allow building with
-    # a newer DMD
-    (fetchpatch {
-      url = "https://github.com/dlang/dmd/commit/c4d33e5eb46c123761ac501e8c52f33850483a8a.patch";
-      stripLen = 1;
-      extraPrefix = "dmd/";
-      sha256 = "sha256-N21mAPfaTo+zGCip4njejasraV5IsWVqlGR5eOdFZZE=";
-    })
-  ] ++ lib.optionals (lib.versionOlder version "2.092.2") [
-    # Fixes C++ tests that compiled on older C++ but not on the current one
-    (fetchpatch {
-      url = "https://github.com/dlang/druntime/commit/438990def7e377ca1f87b6d28246673bb38022ab.patch";
-      stripLen = 1;
-      extraPrefix = "druntime/";
-      sha256 = "sha256-/pPKK7ZK9E/mBrxm2MZyBNhYExE8p9jz8JqBdZSE6uY=";
-    })
-  ];
+    postPatch =
+      ''
+        patchShebangs dmd/test/{runnable,fail_compilation,compilable,tools}{,/extra-files}/*.sh
 
-  postPatch = ''
-    patchShebangs dmd/test/{runnable,fail_compilation,compilable,tools}{,/extra-files}/*.sh
+        rm dmd/test/runnable/gdb1.d
+        rm dmd/test/runnable/gdb10311.d
+        rm dmd/test/runnable/gdb14225.d
+        rm dmd/test/runnable/gdb14276.d
+        rm dmd/test/runnable/gdb14313.d
+        rm dmd/test/runnable/gdb14330.d
+        rm dmd/test/runnable/gdb15729.sh
+        rm dmd/test/runnable/gdb4149.d
+        rm dmd/test/runnable/gdb4181.d
 
-    rm dmd/test/runnable/gdb1.d
-    rm dmd/test/runnable/gdb10311.d
-    rm dmd/test/runnable/gdb14225.d
-    rm dmd/test/runnable/gdb14276.d
-    rm dmd/test/runnable/gdb14313.d
-    rm dmd/test/runnable/gdb14330.d
-    rm dmd/test/runnable/gdb15729.sh
-    rm dmd/test/runnable/gdb4149.d
-    rm dmd/test/runnable/gdb4181.d
+        # Disable tests that rely on objdump whitespace until fixed upstream:
+        #   https://issues.dlang.org/show_bug.cgi?id=23317
+        rm dmd/test/runnable/cdvecfill.sh
+        rm dmd/test/compilable/cdcmp.d
 
-    # Disable tests that rely on objdump whitespace until fixed upstream:
-    #   https://issues.dlang.org/show_bug.cgi?id=23317
-    rm dmd/test/runnable/cdvecfill.sh
-    rm dmd/test/compilable/cdcmp.d
+        # Grep'd string changed with gdb 12
+        #   https://issues.dlang.org/show_bug.cgi?id=23198
+        substituteInPlace druntime/test/exceptions/Makefile \
+          --replace 'in D main (' 'in _Dmain ('
 
-    # Grep'd string changed with gdb 12
-    #   https://issues.dlang.org/show_bug.cgi?id=23198
-    substituteInPlace druntime/test/exceptions/Makefile \
-      --replace 'in D main (' 'in _Dmain ('
+        # We're using gnused on all platforms
+        substituteInPlace druntime/test/coverage/Makefile \
+          --replace 'freebsd osx' 'none'
+      ''
+      + lib.optionalString (lib.versionOlder version "2.091.0") ''
+        # This one has tested against a hardcoded year, then against a current year on
+        # and off again. It just isn't worth it to patch all the historical versions
+        # of it, so just remove it until the most recent change.
+        rm dmd/test/compilable/ddocYear.d
+      ''
+      + lib.optionalString (lib.versionAtLeast version "2.089.0" && lib.versionOlder version "2.092.2") ''
+        rm dmd/test/dshell/test6952.d
+      ''
+      + lib.optionalString (lib.versionAtLeast version "2.092.2") ''
+        substituteInPlace dmd/test/dshell/test6952.d --replace "/usr/bin/env bash" "${bash}/bin/bash"
+      ''
+      + lib.optionalString stdenv.isLinux ''
+        substituteInPlace phobos/std/socket.d --replace "assert(ih.addrList[0] == 0x7F_00_00_01);" ""
+      ''
+      + lib.optionalString stdenv.isDarwin ''
+        substituteInPlace phobos/std/socket.d --replace "foreach (name; names)" "names = []; foreach (name; names)"
+      '';
 
-    # We're using gnused on all platforms
-    substituteInPlace druntime/test/coverage/Makefile \
-      --replace 'freebsd osx' 'none'
-  ''
+    nativeBuildInputs =
+      [
+        makeWrapper
+        which
+        installShellFiles
+      ]
+      ++ lib.optionals (lib.versionOlder version "2.088.0") [
+        git
+      ];
 
-  + lib.optionalString (lib.versionOlder version "2.091.0") ''
-    # This one has tested against a hardcoded year, then against a current year on
-    # and off again. It just isn't worth it to patch all the historical versions
-    # of it, so just remove it until the most recent change.
-    rm dmd/test/compilable/ddocYear.d
-  '' + lib.optionalString (lib.versionAtLeast version "2.089.0" && lib.versionOlder version "2.092.2") ''
-    rm dmd/test/dshell/test6952.d
-  '' + lib.optionalString (lib.versionAtLeast version "2.092.2") ''
-    substituteInPlace dmd/test/dshell/test6952.d --replace "/usr/bin/env bash" "${bash}/bin/bash"
-  ''
+    buildInputs =
+      [
+        curl
+        tzdata
+      ]
+      ++ lib.optionals stdenv.isDarwin [
+        Foundation
+      ];
 
-  + lib.optionalString stdenv.isLinux ''
-    substituteInPlace phobos/std/socket.d --replace "assert(ih.addrList[0] == 0x7F_00_00_01);" ""
-  '' + lib.optionalString stdenv.isDarwin ''
-    substituteInPlace phobos/std/socket.d --replace "foreach (name; names)" "names = []; foreach (name; names)"
-  '';
+    nativeCheckInputs =
+      [
+        gdb
+      ]
+      ++ lib.optionals (lib.versionOlder version "2.089.0") [
+        unzip
+      ];
 
-  nativeBuildInputs = [
-    makeWrapper
-    which
-    installShellFiles
-  ] ++ lib.optionals (lib.versionOlder version "2.088.0") [
-    git
-  ];
+    buildFlags = [
+      "BUILD=release"
+      "ENABLE_RELEASE=1"
+      "PIC=1"
+    ];
 
-  buildInputs = [
-    curl
-    tzdata
-  ] ++ lib.optionals stdenv.isDarwin [
-    Foundation
-  ];
+    # Build and install are based on http://wiki.dlang.org/Building_DMD
+    buildPhase = ''
+      runHook preBuild
 
-  nativeCheckInputs = [
-    gdb
-  ] ++ lib.optionals (lib.versionOlder version "2.089.0") [
-    unzip
-  ];
+      export buildJobs=$NIX_BUILD_CORES
+      if [ -z $enableParallelBuilding ]; then
+        buildJobs=1
+      fi
 
-  buildFlags = [
-    "BUILD=release"
-    "ENABLE_RELEASE=1"
-    "PIC=1"
-  ];
+      make -C dmd -f posix.mak $buildFlags -j$buildJobs HOST_DMD=${HOST_DMD}
+      make -C druntime -f posix.mak $buildFlags -j$buildJobs DMD=${pathToDmd}
+      echo ${tzdata}/share/zoneinfo/ > TZDatabaseDirFile
+      echo ${lib.getLib curl}/lib/libcurl${stdenv.hostPlatform.extensions.sharedLibrary} > LibcurlPathFile
+      make -C phobos -f posix.mak $buildFlags -j$buildJobs DMD=${pathToDmd} DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$PWD"
 
-  # Build and install are based on http://wiki.dlang.org/Building_DMD
-  buildPhase = ''
-    runHook preBuild
+      runHook postBuild
+    '';
 
-    export buildJobs=$NIX_BUILD_CORES
-    if [ -z $enableParallelBuilding ]; then
-      buildJobs=1
-    fi
+    doCheck = true;
 
-    make -C dmd -f posix.mak $buildFlags -j$buildJobs HOST_DMD=${HOST_DMD}
-    make -C druntime -f posix.mak $buildFlags -j$buildJobs DMD=${pathToDmd}
-    echo ${tzdata}/share/zoneinfo/ > TZDatabaseDirFile
-    echo ${lib.getLib curl}/lib/libcurl${stdenv.hostPlatform.extensions.sharedLibrary} > LibcurlPathFile
-    make -C phobos -f posix.mak $buildFlags -j$buildJobs DMD=${pathToDmd} DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$PWD"
+    checkFlags = buildFlags;
 
-    runHook postBuild
-  '';
+    # many tests are disbled because they are failing
 
-  doCheck = true;
+    # NOTE: Purity check is disabled for checkPhase because it doesn't fare well
+    # with the DMD linker. See https://github.com/NixOS/nixpkgs/issues/97420
+    checkPhase = ''
+      runHook preCheck
 
-  checkFlags = buildFlags;
+      export checkJobs=$NIX_BUILD_CORES
+      if [ -z $enableParallelChecking ]; then
+        checkJobs=1
+      fi
 
-  # many tests are disbled because they are failing
+      NIX_ENFORCE_PURITY= \
+        make -C dmd/test $checkFlags CC=$CXX SHELL=$SHELL -j$checkJobs N=$checkJobs
 
-  # NOTE: Purity check is disabled for checkPhase because it doesn't fare well
-  # with the DMD linker. See https://github.com/NixOS/nixpkgs/issues/97420
-  checkPhase = ''
-    runHook preCheck
+      NIX_ENFORCE_PURITY= \
+        make -C druntime -f posix.mak unittest $checkFlags -j$checkJobs
 
-    export checkJobs=$NIX_BUILD_CORES
-    if [ -z $enableParallelChecking ]; then
-      checkJobs=1
-    fi
+      NIX_ENFORCE_PURITY= \
+        make -C phobos -f posix.mak unittest $checkFlags -j$checkJobs DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$PWD"
 
-    NIX_ENFORCE_PURITY= \
-      make -C dmd/test $checkFlags CC=$CXX SHELL=$SHELL -j$checkJobs N=$checkJobs
+      runHook postCheck
+    '';
 
-    NIX_ENFORCE_PURITY= \
-      make -C druntime -f posix.mak unittest $checkFlags -j$checkJobs
+    installPhase = ''
+      runHook preInstall
 
-    NIX_ENFORCE_PURITY= \
-      make -C phobos -f posix.mak unittest $checkFlags -j$checkJobs DFLAGS="-version=TZDatabaseDir -version=LibcurlPath -J$PWD"
+      install -Dm755 ${pathToDmd} $out/bin/dmd
 
-    runHook postCheck
-  '';
+      installManPage dmd/docs/man/man*/*
 
-  installPhase = ''
-    runHook preInstall
+      mkdir -p $out/include/dmd
+      cp -r {druntime/import/*,phobos/{std,etc}} $out/include/dmd/
 
-    install -Dm755 ${pathToDmd} $out/bin/dmd
+      mkdir $out/lib
+      cp phobos/generated/${osname}/release/${bits}/libphobos2.* $out/lib/
 
-    installManPage dmd/docs/man/man*/*
+      wrapProgram $out/bin/dmd \
+        --prefix PATH ":" "${targetPackages.stdenv.cc}/bin" \
+        --set-default CC "${targetPackages.stdenv.cc}/bin/cc"
 
-    mkdir -p $out/include/dmd
-    cp -r {druntime/import/*,phobos/{std,etc}} $out/include/dmd/
+      substitute ${dmdConfFile} "$out/bin/dmd.conf" --subst-var out
 
-    mkdir $out/lib
-    cp phobos/generated/${osname}/release/${bits}/libphobos2.* $out/lib/
+      runHook postInstall
+    '';
 
-    wrapProgram $out/bin/dmd \
-      --prefix PATH ":" "${targetPackages.stdenv.cc}/bin" \
-      --set-default CC "${targetPackages.stdenv.cc}/bin/cc"
-
-    substitute ${dmdConfFile} "$out/bin/dmd.conf" --subst-var out
-
-    runHook postInstall
-  '';
-
-  meta = with lib; {
-    description = "Official reference compiler for the D language";
-    homepage = "https://dlang.org/";
-    # Everything is now Boost licensed, even the backend.
-    # https://github.com/dlang/dmd/pull/6680
-    license = licenses.boost;
-    maintainers = with maintainers; [ ThomasMader lionello dukc ];
-    platforms = [ "x86_64-linux" "i686-linux" "x86_64-darwin" ];
-  };
-}
+    meta = with lib; {
+      description = "Official reference compiler for the D language";
+      homepage = "https://dlang.org/";
+      # Everything is now Boost licensed, even the backend.
+      # https://github.com/dlang/dmd/pull/6680
+      license = licenses.boost;
+      maintainers = with maintainers; [ThomasMader lionello dukc];
+      platforms = ["x86_64-linux" "i686-linux" "x86_64-darwin"];
+    };
+  }
