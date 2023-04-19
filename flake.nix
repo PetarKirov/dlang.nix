b33b21a45f6567a3e40d7c3dd9e2515cab8726d6
Petar Kirov <petar.p.kirov@gmail.com>
2023-04-19 18:24:21 +0300

    feat(shell.nix): Use pkgs instance resulting from applying the flake overlay
    This way we can access all packages defined in this flake in the devshell.

--- flake.nix
+++ flake.nix
@@ -20,8 +20,8 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       imports = [./pkgs];
-      perSystem = {pkgs, ...}: {
-        devShells.default = import ./shell.nix {inherit pkgs;};
+      perSystem = {final, ...}: {
+        devShells.default = import ./shell.nix {pkgs = final;};
       };
     };
 }
