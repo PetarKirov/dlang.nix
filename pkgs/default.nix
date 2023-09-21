38748cb40275a4cfc2b98523c1ca4c13a6920ef0
Petar Kirov <petar.p.kirov@gmail.com>
2023-09-21 22:48:42 +0300

    feat(pkgs/dmd): Automate definition of package versions
    * Move hashes for each supported version to machine-generated JSON files
    * Automatically generate derivations per each supported source and binary
      version

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,11 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: let
+  inherit (builtins) attrNames listToAttrs map mapAttrs;
+  inherit (lib) nameValuePair pipe optionalAttrs;
+in {
   imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
   perSystem = {
@@ -6,12 +13,59 @@
     pkgs,
     ...
   }: let
-    inherit (pkgs) callPackage lib darwin hostPlatform;
+    inherit (pkgs) callPackage hostPlatform;
     darwinPkgs = {
-      inherit (darwin.apple_sdk.frameworks) Foundation;
+      inherit (pkgs.darwin.apple_sdk.frameworks) Foundation;
+    };
+
+    genPkgVersions = pkgName: let
+      mod = ./. + "/${pkgName}/supported-versions.nix";
+      inherit
+        (import mod)
+        supportedVersions
+        getSourceVersion
+        getBinaryVersion
+        ;
+      getVersion = type:
+        if type == "source"
+        then getSourceVersion
+        else getBinaryVersion;
+    in {
+      flattened = type: let
+        nameSuffix =
+          if type == "binary"
+          then "-binary"
+          else "";
+      in
+        pipe (attrNames supportedVersions."${type}") [
+          (
+            map (
+              version:
+                nameValuePair
+                "${pkgName}${nameSuffix}-${version}"
+                (callPackage (getVersion type version) darwinPkgs)
+            )
+          )
+          listToAttrs
+        ];
+
+      hierarchical = {
+        "${pkgName}" = pipe ["source" "binary"] [
+          (map (type:
+            nameValuePair type (
+              mapAttrs
+              (version: _: (callPackage (getVersion type version) darwinPkgs))
+              supportedVersions."${type}"
+            )))
+          listToAttrs
+        ];
+      };
     };
   in {
     overlayAttrs = self'.packages;
+    legacyPackages =
+      {}
+      // (genPkgVersions "dmd").hierarchical;
     packages =
       {
         ldc-binary = callPackage ./ldc/bootstrap.nix {};
@@ -19,9 +73,13 @@
 
         dub = callPackage ./dub {};
       }
-      // lib.optionalAttrs hostPlatform.isx86 {
-        dmd-binary = callPackage ./dmd/bootstrap.nix {};
-        dmd = callPackage ./dmd darwinPkgs;
-      };
+      // optionalAttrs hostPlatform.isx86 (
+        {
+          dmd-bootstrap = self'.packages."dmd-binary-2.098.0";
+          dmd = self'.packages."dmd-2.100.2";
+        }
+        // (genPkgVersions "dmd").flattened "binary"
+        // (genPkgVersions "dmd").flattened "source"
+      );
   };
 }
