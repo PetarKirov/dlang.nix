631d09770ec48c6df1f94b87b695bdc748c90eb7
Petar Kirov <petar.p.kirov@gmail.com>
2023-09-25 02:12:06 +0300

    improve(scripts/fetch_binary.d): Print result as json
    * style: re-order imports
    * fix: handling of `--help` and invalid cmd-line args
    * improve: pass-through stderr of child processes
    * improve: separate logging vs result output by using stderr and stdout as
      appropriate

--- scripts/fetch_binary.d
+++ scripts/fetch_binary.d
@@ -1,10 +1,14 @@
 #!/usr/bin/env -S rdmd -preview=shortenedMethods
 
-import std.algorithm : map, startsWith;
+import std.algorithm : map, startsWith, joiner;
+import std.array : array;
 import std.format : format;
-import std.stdio : writeln, writefln;
-import std.getopt : getopt, defaultGetoptPrinter;
+import std.getopt : getopt, GetOptException, defaultGetoptFormatter;
+static import std.getopt;
+import std.json : JSONValue, JSONOptions;
 import std.parallelism : parallel;
+import std.process : executeShell, Config;
+import std.stdio : stdout, stderr;
 import std.string : strip;
 import std.typecons : tuple;
 
@@ -22,80 +26,118 @@ string suffix(Platform p) => p.startsWith("windows") ? "7z" : "tar.xz";
 
 enum CompilerInfo[Compiler] supportedPlatforms = [
     Compiler.dmd: CompilerInfo(
-        (platform, compilerVersion) => "http://downloads.dlang.org/releases/2.x/%s/dmd.%s.%s.%s"
-            .format(compilerVersion, compilerVersion, platform, suffix(platform)),
-        [
-            "linux", "osx", "freebsd-64", "windows"
-        ],
+        (platform, compilerVersion) =>
+            "http://downloads.dlang.org/releases/2.x/%s/dmd.%s.%s.%s"
+                .format(compilerVersion, compilerVersion, platform, suffix(platform)),
+            [
+                "linux", "osx", "freebsd-64", "windows"
+            ],
     ),
     Compiler.ldc: CompilerInfo(
-        (platform, compilerVersion) => "https://github.com/ldc-developers/ldc/releases/download/v%s/ldc2-%s-%s.%s"
-            .format(compilerVersion, compilerVersion, platform, suffix(platform)),
-        [
-            "android-aarch64", "android-armv7a",
-            "freebsd-x86_64",
-            "linux-aarch64", "linux-x86_64",
-            "osx-arm64", "osx-x86_64",
-            "windows-x64", "windows-x86"
-        ],
+        (platform, compilerVersion) =>
+            "https://github.com/ldc-developers/ldc/releases/download/v%s/ldc2-%s-%s.%s"
+                .format(compilerVersion, compilerVersion, platform, suffix(platform)),
+            [
+                "android-aarch64", "android-armv7a",
+                "freebsd-x86_64",
+                "linux-aarch64", "linux-x86_64",
+                "osx-arm64", "osx-x86_64",
+                "windows-x64", "windows-x86"
+            ],
     ),
 ];
 
 void main(string[] args) {
-    Version compilerVersion;
+    Version[] compilerVersions;
     Compiler compiler = Compiler.ldc;
     bool dryRun = true;
 
-    auto help = args.getopt(
-        "version", &compilerVersion,
-        "compiler", &compiler,
-        "dry-run", &dryRun,
-    );
+    auto parseCLI(string[] args) {
+        std.getopt.arraySep = ",";
+        return args.getopt(
+            "versions", &compilerVersions,
+            "compiler", &compiler,
+            "dry-run", &dryRun,
+        );
+    }
 
-    compilerVersion = compilerVersion
-        ? compilerVersion
+    bool helpWanted = false;
+    try {
+        auto cli = parseCLI(args);
+        helpWanted = cli.helpWanted;
+    } catch (GetOptException) {
+        helpWanted = true;
+    }
+
+    if (helpWanted) {
+        auto w = stderr.lockingTextWriter();
+        defaultGetoptFormatter(
+            w,
+            "fetch_binary.d - " ~
+                "tool for fetching binary releases of DMD and LDC",
+            parseCLI(args[0 .. 1]).options,
+        );
+        return;
+    }
+
+    compilerVersions = compilerVersions.length
+        ? compilerVersions
         : compiler == Compiler.ldc
-        ? "1.34.0"
-        : "2.105.0";
-
-    if (help.helpWanted)
-        defaultGetoptPrinter("Some information about the program.", help.options);
+        ? [ "1.34.0" ]
+        : [ "2.105.0" ];
 
     const compilerInfo = supportedPlatforms[compiler];
     const platforms = compilerInfo.platforms;
     const getUrl = compilerInfo.urlFormatter;
 
-    writefln("Prefetching %s v%s for %s:", compiler, compilerVersion, platforms);
-    writeln("-----");
+    foreach (compilerVersion; compilerVersions)
+        stderr.writefln(
+            "* Prefetching %s v%s for %s:",
+            compiler,
+            compilerVersion,
+            platforms,
+        );
+    stderr.writeln("-----");
 
-    Hash[Platform] hashes;
+    Hash[Platform][Version] hashes;
 
-    foreach (platform; platforms.parallel) {
-        const url = getUrl(platform, compilerVersion);
-        if (const hash = prefech(dryRun, url))
-            hashes[platform] = hash;
+    auto jobs = compilerVersions.map!(compilerVersion =>
+        platforms.map!(platform =>
+            tuple(compilerVersion, platform, getUrl(platform, compilerVersion))
+        ).array
+    ).joiner.array;
+
+    foreach (job; jobs.parallel) {
+        const compilerVersion = job[0], platform = job[1], url = job[2];
+        hashes[compilerVersion][platform] = prefech(dryRun, url);
     }
 
-    writeln("-----");
-    writeln("All done!\n");
+    if (dryRun) {
+        stderr.writeln("-----");
+        stderr.writeln("Fetching was not performed, this was a dry run.");
+    }
+    stderr.writeln("-----");
+    stderr.writeln("All done!\n");
 
-    foreach (platform, hash; platforms.map!(p => tuple(p, p in hashes)))
-       `"%s" = "%s";`.writefln(
-            platform,
-            hash ? *hash : "<not available>"
-        );
+    stdout.writeln(
+        hashes.JSONValue.toPrettyString(JSONOptions.doNotEscapeSlashes)
+    );
 }
 
 Hash prefech(bool dryRun, Url url) =>
     executeCommand(
         dryRun,
-        `nix store prefetch-file --json "%s" | jq -r '.hash'`.format(url)
+        `nix store prefetch-file --json "%s" | jq -r '.hash'`
+            .format(url)
     ).strip;
 
 string executeCommand(bool dryRun, string command) {
-    `> %s`.writefln(command);
-    import std.process : executeShell;
+    stderr.writefln(`> %s`, command);
     if (dryRun) return null;
-    const result = executeShell(command);
+    const result = executeShell(
+      command,
+      null,
+      Config.stderrPassThrough,
+    );
     return result.status == 0 ? result.output : null;
 }
