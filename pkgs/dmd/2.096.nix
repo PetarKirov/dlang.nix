59bbdf6cf289757708382743e93815ca5a032890
Ate Eskola <ajieskola@gmail.com>
2023-10-14 09:59:30 +0300

    feat(pkgs/dmd): Version 2.096.1 supported

---
+++ pkgs/dmd/2.096.nix
@@ -0,0 +1,7 @@
+import ./generic.nix {
+  version = "2.096.1";
+  dmdSha256 = "04s5z2skhvjjxwpgdffgksk6whd2jsyrqb5f57icp1qvm1ywz1vb";
+  druntimeSha256 = "1jzs6qrljs7nvq0dlld9l7f5n2yj306i5czpvxgxymbrcyzzh1q4";
+  phobosSha256 = "0jyxkdy6h35z1cv22pqhx136mcpg8h2hwxr4j4lixpzyr8kw0i4d";
+  toolsSha256 = "sha256-uz0sWHPtXhU/pI9/+o0PxrpZpEA/AZoTAxawf7TIqgg=";
+}
