d5da1a54c2122832d19c09dd447080e270d9c272
Petar Kirov <petar.p.kirov@gmail.com>
2024-01-28 19:17:03 +0200

    docs(readme): Initial pass over the docs

---
+++ templates/devshell/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My D project";
+
+  inputs = {
+    dlang-nix.url = "github:PetarKirov/dlang-nix";
+
+    nixpkgs.follows = "dlang-nix/nixpkgs";
+
+    # Check https://flake.parts/ for more info
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+      perSystem = {
+        inputs',
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.figlet
+            inputs'.dlang-nix.packages.ldc
+            inputs'.dlang-nix.packages.dub
+          ];
+
+          shellHook = ''
+            figlet "Hello, D world!"
+          '';
+        };
+      };
+    };
+}
