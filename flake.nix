89cb2227525c6342894bb3c3484310712487229c
Petar Kirov <petar.p.kirov@gmail.com>
2024-01-28 20:04:43 +0200

    build(flake.nix/inputs): Add `flake-compat`
    • Added input 'flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33' (2023-10-04)

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
+    flake-compat.url = "github:edolstra/flake-compat";
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
