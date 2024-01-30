5f0649129f1328eb97da5a9c17e346786da4c786
Petar Kirov <petar.p.kirov@gmail.com>
2024-01-30 20:47:54 +0200

    ci(gh-actions): Switch Darwin jobs from `macos-14` to `flyci-macos-large-latest-m1`

--- lib/mk-gh-actions-matrix.nix
+++ lib/mk-gh-actions-matrix.nix
@@ -6,11 +6,17 @@
   flake = {
     lib = rec {
       # See https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners#standard-github-hosted-runners-for-public-repositories
+      # See https://www.flyci.net/#pricing
       nixSystemToGHPlatform = {
+        # GH-hosted runners:
         "x86_64-linux" = "ubuntu-latest";
         # "x86_64-darwin" = "macos-13"; - macos-13 is a 4 x86_64 vCPU / 14GB RAM
-        "x86_64-darwin" = "macos-14"; # - macos-14 is a 3 aarch64 vCPU / 7GB RAM (but it seems faster than the macos-13 one)
-        "aarch64-darwin" = "macos-14";
+        # "x86_64-darwin" = "macos-14"; # - macos-14 is a 3 aarch64 vCPU / 7GB RAM (but it seems faster than the macos-13 one)
+        # "aarch64-darwin" = "macos-14";
+
+        # FlyCI-hosted runners:
+        "x86_64-darwin" = "flyci-macos-large-latest-m1";
+        "aarch64-darwin" = "flyci-macos-large-latest-m1";
       };
 
       inherit (import ./build-status.nix {inherit lib;}) getBuildStatus;
