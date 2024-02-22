d7fedd89dd4d783aa20c63bb79a199d8ad02d19d
Ate Eskola <ajieskola@gmail.com>
2024-02-22 14:27:53 +0200

    feat(pkgs/dmd): Made LDC the default bootstrap compiler.

--- pkgs/dmd/bootstrap.nix
+++ pkgs/dmd/bootstrap.nix
@@ -1,10 +1,14 @@
-import ./binary.nix {
-  version = "2.098.0";
+import ../ldc/binary.nix {
+  version = "1.25.0";
   hashes = {
-    # COMPILER=dmd VERSION=2.098.0 ./scripts/fetch-binary
-    "linux" = "sha256-EQTl5Z/UeCi3mNd6cr5Ue/CGu6HTdKGFXGtYFMTbAUU=";
-    "osx" = "sha256-d4Cq1EKdSZpkfn6Qdwb3dWVr539EJci0rqt5gCTH80I=";
-    "freebsd-64" = "sha256-c7ODMpI/kGdl3R4a7/gSTDzTcD3hbHtl4teHJvT0IGk=";
-    "windows" = "sha256-Yhw3hkkVdvHTayxdk/XNKZwrV8cDMfz63rgBqark+gU=";
+    "android-aarch64" = "sha256-kTlEPW3cLTze80bIxZ2LOvghcGeCiZkLj4aZYt6ozkc=";
+    "android-armv7a" = "sha256-4jN03bFJ+aNHRBkZUOcBik54WybchdBENcQi41sdheg=";
+    "freebsd-x86_64" = "sha256-s7EcW1CfjMY92yA3TsojvD+r08o4qmL/sBj6QWkZeoo=";
+    "linux-aarch64" = "sha256-UDZ43x4flSo+SfsPeE8juZO2Wtk2ZzwySk0ADHnvJBI=";
+    "linux-x86_64" = "sha256-sfg47RdlsIpryc3iZvE17OtLweh3Zw6DeuNJYgpuH+o=";
+    "osx-arm64" = "sha256-O/x0vy0wwQFaDc4uWSeMhx+chJKqbQb6e5QNYf+7DCw=";
+    "osx-x86_64" = "sha256-6iKnbS+oalLKmyS8qYD/wS21b7+O+VgsWG2iT4PrWPU=";
+    "windows-x64" = "sha256-Yc9vg6Uuw7Sn8Z+CfoCsgM8KqxvezS26h1NROsHWfd0=";
+    "windows-x86" = "sha256-iR28Yh+nQbCZvq8Lrey7ryvUq4ajsS27nXMsienFPcY=";
   };
 }
