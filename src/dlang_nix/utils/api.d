a701a63fd84c2aaa6cffe24353cab46f2fa6b549
Petar Kirov <petar.p.kirov@gmail.com>
2024-05-18 16:18:58 +0300

    refactor(dlang-nix-fetcher): Move code into reusable modules

---
+++ src/dlang_nix/utils/api.d
@@ -0,0 +1,71 @@
+module dlang_nix.utils.api;
+
+import std.algorithm : find, map, splitter, startsWith;
+import std.array : array;
+import std.string : strip;
+import std.typecons : Tuple;
+
+import dlang_nix.utils.ranges : firstOrDefault, tryGet;
+
+alias Deserialize(T) = T[] function(const(char)[] response);
+alias NextPage(T) = string function(
+    T[] data,
+    string[string] httpResponseHeaders,
+);
+
+T[] fetchPagedResponse(T)(
+    string firstPage,
+    string[string] requestHeaders,
+    Deserialize!T deserialize,
+    NextPage!T getNextPage,
+)
+{
+    import std.net.curl : get, HTTP;
+
+    T[] result;
+    int page = 0;
+    bool moreData = false;
+    auto nextPage = firstPage;
+
+    auto client = HTTP();
+
+    foreach (key, value; requestHeaders)
+        client.addRequestHeader(key, value);
+
+    do
+    {
+        auto response = deserialize(get(nextPage, client));
+        result ~= response;
+        nextPage = getNextPage(response, client.responseHeaders);
+    }
+    while (nextPage);
+
+    return result;
+}
+
+string getNextPageFromLinkHeader(string[string] headers)
+{
+    auto linkHeader = headers.tryGet("Link", "link");
+    if (!linkHeader)
+        return null;
+    return parseLinkHeader(*linkHeader)
+        .firstOrDefault!(l => l.rel == "next")
+        .url;
+}
+
+alias Link = Tuple!(string, "url", string, "rel");
+
+Link[] parseLinkHeader(string header)
+{
+    return header
+        .split(",")
+        .map!(x => x.split(";").map!(part => part.strip).array)
+        .map!(parts => Link(
+            parts[0][1 .. $ - 1], // "<URL>"" -> "URL"
+            parts[1 .. $]
+                .find!((string p) => p.startsWith("rel"))
+                .map!(p => p[5 .. $ - 1]) // `rel="value"` -> `"value"`
+                .firstOrDefault!"!!a"
+        ))
+        .array;
+}
---
+++ src/dlang_nix/utils/github_api.d
@@ -0,0 +1,29 @@
+module dlang_nix.utils.github_api;
+
+import std;
+import std.json;
+import std.format : format;
+
+import semver : SemVer, VersionPart;
+
+import dlang_nix.utils.api : fetchPagedResponse, getNextPageFromLinkHeader;
+
+string[] getGitHubRepoTags(string repo, bool includePrereleases = false)
+{
+    string getGHPage(int page)
+    {
+        return "https://api.github.com/repos/%s/tags?per_page=100&page=%s"
+            .format(repo, page);
+    }
+
+    return fetchPagedResponse!(JSONValue)(
+        getGHPage(1),
+        [ "Authorization": "Bearer " ~ environment["GH_TOKEN"]],
+        (const(char)[] rawResponse) => rawResponse.parseJSON.array,
+        (response, headers) => getNextPageFromLinkHeader(headers),
+    )
+        .map!(j => j.object["name"].str)
+        .filter!(ver => SemVer(ver).isValid)
+        .filter!(ver => includePrereleases || SemVer(ver).isStable)
+        .array;
+}
